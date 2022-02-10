ef4c0681bb25a9686a20c40918b7ba3fe6227d74
johannadznd <johanna.dezarnaud@ynov.com>
2022-02-10 11:54:15 +0100

    feat: add structure

---
+++ lib/views/weather.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Mes todos"),),
+      body: const Text('toto')
+    );
+  }
+}
