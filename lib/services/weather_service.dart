64a4f3a23e9332936f1b3383ce639c83e0c8cb60
johannadznd <johanna.dezarnaud@ynov.com>
2022-02-21 14:01:19 +0100

    feat: get weather with api

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:geocoding/geocoding.dart';
+import 'package:http/http.dart' as http;
+import 'package:ynov_weather/models/meteo.dart';
+
+Future<Meteo> getWeather(name) async {
+  List<Location> coords = await locationFromAddress(name);
+
+  Uri url = Uri.https("api.openweathermap.org", "/data/2.5/weather", {
+    'lat': coords[0].latitude.toString(),
+    'lon': coords[0].longitude.toString(),
+    'lang': 'fr',
+    'appid': 'f46840b0403f41348c5ee528a73851de'
+  });
+
+  var response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    var jsonResponse = jsonDecode(response.body);
+
+
+
+    Weather weather = Weather(
+        id: jsonResponse['weather'][0]['id'],
+        main: jsonResponse['weather'][0]['main'],
+        description: jsonResponse['weather'][0]['description'],
+        icon: jsonResponse['weather'][0]['icon']);
+
+
+       Meteo meteo = Meteo(
+        coord: Coord(
+            lon: jsonResponse['coord']['lon'],
+            lat: jsonResponse['coord']['lat']),
+        base: jsonResponse['base'],
+        main: Main(
+            temp: jsonResponse['main']['temp'],
+            feelsLike: jsonResponse['main']['feels_like'],
+            tempMin: jsonResponse['main']['temp_min'],
+            tempMax: jsonResponse['main']['temp_max'],
+            pressure: jsonResponse['main']['pressure'],
+            humidity: jsonResponse['main']['humidity']),
+        visibility: jsonResponse['visibility'],
+        wind: Wind(
+            speed: jsonResponse['wind']['speed'],
+            deg: jsonResponse['wind']['deg']),
+        clouds: Clouds(all: jsonResponse['clouds']['all']),
+        dt : jsonResponse['dt'],
+        sys : Sys(type : jsonResponse['sys']['type'], id :  jsonResponse['sys']['id'], message : jsonResponse['sys']['message'],country : jsonResponse['sys']['country'] , sunrise : jsonResponse['sys']['sunrise'], sunset :  jsonResponse['sys']['sunset']),
+        timezone : jsonResponse['timezone'],
+        id : jsonResponse['id'],
+        name : jsonResponse['name'],
+        cod : jsonResponse['cod'],
+        weather : List<Weather>.filled(1,weather),
+        );
+    print(meteo.toJson());
+
+    return meteo;
+  } else {
+    print('Request failed : ${response.statusCode}');
+    return Meteo();
+  }
+}
--- lib/views/weather.dart
+++ lib/views/weather.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:ynov_weather/models/meteo.dart';
+import 'package:ynov_weather/services/weather_service.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,7 +10,29 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Mes todos"),),
-      body: const Text('toto')
+      body: Center(
+        child : Column(
+          children: [
+            FutureBuilder<Meteo>(
+              future: getWeather('Paris'),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: Text("Chargement en cours ..."));
+                } else if (snapshot.connectionState == ConnectionState.done) {
+                  return ListTile(
+                    title: const Text('tet'),
+                    subtitle: Text(snapshot.data!.coord!.lat.toString()),
+                    trailing: Text(snapshot.data!.coord!.lon.toString()),
+                  );
+                } else {
+                  return const Text("Une Erreur est survenue");
+                }
+              },
+            )
+          ],
+        ),
+         
+      )
     );
   }
 }
